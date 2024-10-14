db14f6ebf4d5b673691403614e64b61f4cbf4a6e
kapilsharma432001 <sharmakapil432001@gmail.com>
2024-10-14 14:29:51 +0530

    Writing first custom command

---
+++ dataentry/management/commands/helloworld.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "This command is used to print the hello world."
+
+    def handle(self, *args, **kwargs):
+        print("Hello, World!")
